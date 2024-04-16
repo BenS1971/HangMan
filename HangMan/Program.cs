21eaf81b6c7b912850077d5edad81ae8a404cec4
Ben Strycker <bgstrycker1971@gmail.com>
2024-04-15 20:29:28 -0400

    inital commit

---
+++ HangMan/Program.cs
@@ -0,0 +1,15 @@
+﻿ using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+     {
+        // I'll put a word array here. 
+       string [] arr = {"cat","dog","car","bird","girl","boy"};
+        //This is how the array will be used.
+        
+    }
+}
+
+ 
