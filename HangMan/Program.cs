d3e68cfab75efd381bc9ab5c0180ef2873509233
Ben Strycker <bgstrycker1971@gmail.com>
2024-04-15 20:51:58 -0400

    Hit it.

--- HangMan/Program.cs
+++ HangMan/Program.cs
@@ -8,6 +8,15 @@ class Program
         // I'll put a word array here. 
        string [] arr = {"cat","dog","car","bird","girl","boy"};
         //This is how the array will be used.
+        Random random = new Random();
+        //This will randomize a three or four-lettered word for the game.
+
+        var 
+        while 
+        {
+            Console.WriteLine("" + random.Next);
+        }
+
         
     }
 }
